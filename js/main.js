4bca543800876134f8ee4d9c419d7b08b4a357f4
NicoRojo562 <2000262@ucc.edu.ar>
2021-06-14 13:15:49 -0300

    cambios js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
+function Validar(Nombre) {
+    if (/^[A-Z]+$/i.test(Nombre)) {
+        return true;
+    } else {
+        return false;
+    }
+} let name;
+
 function miFunc (){
 
     let carrera, anio;
    carrera = document.getElementById("Carreras").value;
     anio=document.getElementById("Año").value;
-    if (carrera==0 && anio==0) {alert('Seleccione las opciones correspondientes')}
-    else if(carrera==1 ){
+     name=document.getElementById("nombre").value;
+    if (carrera==0 && anio==0 && Validar(name)==false) {alert('Seleccione las opciones correspondientes')}
+    else if (Validar(name)==false){
+        document.getElementById("nombre").focus();
+        alert('ingrese un nombre  valido');
+    }
+    else if(carrera==1 && Validar(name)){
       if(anio==1){
             window.open("Ing%20en%20computacion/ingcomputacion.html")
         } else if (anio==2) { window.open("ing%20computacion%202/ingcomputacion2.html")}
       else if(anio!=1 || anio!=2) {alert('Seleccione el año porfavor')}
     }
-    else if(carrera==2) {
+    else if(carrera==2 && Validar(name)) {
         if(anio==1){window.open("ingelectronica/Ingelectronica1.html")}
         else if (anio==2){window.open("Ingelectronica%202/ingelectronica2.html")}
         else if(anio!=1 || anio!=2) {alert('Seleccione el año porfavor')}
     }
-    else if(carrera==3){
+    else if(carrera==3 && Validar(name)){
         if(anio==1){window.open("ingensistemas1/ingensistemas1.html")}
         else if (anio==2){window.open("ingensistemas2/ingensistemas2.html")}
         else if(anio!=1 || anio!=2) {alert('Seleccione el año porfavor')}
     }
-    else if (carrera!=1 || carrera != 2){if (carrera!=1 || carrera!=2){alert('Seleccione la carrera porfavor')}}
+    else if (carrera!=1 && carrera != 2 && carrera!=3){alert('Seleccione la carrera porfavor')}
 
 }
+
+
+window.onload= function mostrar(){
+    document.getElementById("texto").innerHTML ="tetas";
+}
+
+window.onload=function draw() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext('2d');
+    ctx.translate(250,200);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+    }
+
+
+
 /**
 function miFunc() {
     const carrera = document.getElementById("carreras");
