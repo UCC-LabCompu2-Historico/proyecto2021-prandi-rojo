359acbb8e7a7293be9d4b9f5c481ed6e6e5c8d34
NicoRojo562 <2000262@ucc.edu.ar>
2021-06-15 02:54:22 -0300

    puliendo detalles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/**
+ * Checkeo de variable nombre, que lo ingresado sean letras.
+ * @method Validar.
+ * @param {string} Nombre - contiene el valor del nombre.
+ * @returns {boolean}.
+ */
 function Validar(Nombre) {
     if (/^[A-Z]+$/i.test(Nombre)) {
         return true;
@@ -5,6 +11,13 @@ function Validar(Nombre) {
         return false;
     }
 } let name;
+
+/**
+ * Controla los ingresos del usuario en el formulario desplegable.
+ * @method miFunc.
+ * No recibe parametros.
+ * No retorna valores.
+ */
 
 function miFunc (){
 
@@ -36,6 +49,14 @@ function miFunc (){
     else if (carrera!=1 && carrera != 2 && carrera!=3){alert('Seleccione la carrera porfavor')}
 
 }
+
+/**
+ * Esta funcion se encarga de mostrar en otra pagina, el nombre ingresado previamente por el usuario.
+ * @method cargarnombre.
+ * No recibe paremtros.
+ * devuelve mensaje en otra pantalla, con informacion adicional y variable del usuario (su nombre).
+ */
+
 function cargarnombre(){
     var urlcomp,nombre;
     urlcomp=window.location.href.split("/")[5];
@@ -44,49 +65,122 @@ function cargarnombre(){
    document.getElementById("texto").innerHTML = "¡Buenos dias " + nombre + " bienvenido a la Universidad Nacional de los Nicos!";
 }
 
+/**
+ * Se encarga de generar el canvas por defecto que aparecera en la pagina al abrirla.
+ * @method cargarcanvas.
+ * No recibe parametros
+ * No retorna valores.
+ */
+
 function cargarcanvas(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    let alto = canvas.height;
+    let ancho = canvas.width;
+
+    var grid_size = 25;
+    var x_axis_size = ancho;
+    var y_axis_size = alto;
+    var x_cero = 250;
+    var y_cero = 200;
+
+    ctx.beginPath();
+    // Cuadriculas X
+    for (var l=0; l<=ancho; l=l+grid_size){
+        ctx.moveTo(l,0);
+        ctx.lineTo(l,alto);
+    }
+
+    // Cuadrículas Y
+    for (var t=0; t<=ancho; t=t+grid_size){
+        ctx.moveTo(0,t);
+        ctx.lineTo(ancho,t);
+    }
+
+    // Mueves el pencil y luego persistes con stroke
+    ctx.strokeStyle = "#dcdcdc";
+    ctx.lineWidth=1;
+    ctx.stroke();
+
+    // Eje x del 0
+    ctx.beginPath();
+    ctx.moveTo(x_cero-(x_axis_size/2),y_cero);
+    ctx.lineTo(x_cero+(x_axis_size/2),y_cero);
+// Eje y del 0
+    ctx.moveTo(x_cero,y_cero-(y_axis_size/2));
+    ctx.lineTo(x_cero,y_cero+(y_axis_size/2));
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth=2;
+    ctx.stroke();
+
+    ctx.font = "bold 10px sans-serif";
+// Los de Y
+    let x = 0;
+    while (x*grid_size <= (y_axis_size/2)) {
+        ctx.textAlign = "end";
+        ctx.fillText(x,x_cero-(grid_size/4),y_cero-(grid_size*x));
+        x++;
+    }
+    x = 1;
+    while (x*grid_size <= (y_axis_size/2)) {
+        ctx.textAlign = "end";
+        ctx.fillText(-x,x_cero-(grid_size/4),y_cero+(grid_size*x));
+        x++;
+    }
+// Los de X
+    x = 1;
+    while (x*grid_size <= (x_axis_size/2)) {
+        ctx.textAlign = "end";
+        ctx.fillText(x,x_cero+(grid_size*x),y_cero+(grid_size/2));
+        x++;
+    }
+    x = 1;
+    while (x*grid_size <= (x_axis_size/2)) {
+        ctx.textAlign = "end";
+        ctx.fillText(-x,x_cero-(grid_size*x),y_cero+(grid_size/2));
+        x++;
+}}
+
+
+
+function funcanva(){
+
+    var x= document.getElementById("x").value;
+    var y =document.getElementById("y").value;
+
+    if (isNaN(x) || isNaN(y)){alert('Valor mal ingresado. Intentelo de nuevo')}
+    else if (x=="" || y==""){alert('Campo no completado')}
+
     var canva=document.getElementById("myCanvas");
     var ctx= canva.getContext("2d");
-
     var altmax= canva.height;
     var anmax=canva.width;
 
     ctx.beginPath();
-    for(var i=0;i<altmax;){
-        ctx.moveTo(0,i);
-        ctx.lineTo(anmax,i);
-        ctx.strokeStyle = "#bbb4b4";
-        ctx.stroke();
-        i=i+20;
-    }
-    ctx.closePath();
-
-
-    ctx.beginPath();
-    for(var i=0;i<anmax;){
-        ctx.moveTo(i,0);
-        ctx.lineTo(i,altmax);
-        ctx.strokeStyle = "#bbb4b4";
-        ctx.stroke();
-        i=i+20;
-    }
-    ctx.closePath();
-
-    //eje X
-    ctx.beginPath();
-        ctx.moveTo(0,altmax/2);
-        ctx.lineTo(anmax,altmax/2);
-        ctx.strokeStyle = "#030303";
-        ctx.stroke();
-    ctx.closePath();
-
-    //eje y
-    ctx.beginPath();
-    ctx.moveTo(anmax/2,0);
-    ctx.lineTo(anmax/2,altmax);
+    ctx.moveTo(0,250-(y*25));
+    ctx.lineTo(anmax,250+x*25);
     ctx.strokeStyle = "#030303";
     ctx.stroke();
     ctx.closePath();
-
-
 }
+
+//ctx.moveTo(0,x*25*(-250)+200-25*y);
+//     ctx.lineTo(anmax,x*25*(anmax)+200-(25*y));
+//(y/x)*25,y*25
+//var x= document.getElementById("x").value;
+//     var y =document.getElementById("y").value;
+//
+//     if (isNaN(x) || isNaN(y)){alert('Valor mal ingresado. Intentelo de nuevo')}
+//     else if (x=="" || y==""){alert('Campo no completado')}
+//
+//     var canva=document.getElementById("myCanvas");
+//     var ctx= canva.getContext("2d");
+//     var altmax= canva.height;
+//     var anmax=canva.width;
+//
+//     ctx.beginPath();
+//     ctx.moveTo(0,250-(y*25));
+//     ctx.lineTo(anmax,250+x*25);
+//     ctx.strokeStyle = "#030303";
+//     ctx.stroke();
+//     ctx.closePath();
