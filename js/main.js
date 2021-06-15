0685d3723f3a721588e7b1d96b096c0de8985397
NicoRojo562 <2000262@ucc.edu.ar>
2021-06-14 22:25:36 -0300

    cambios js

--- js/main.js
+++ js/main.js
@@ -19,55 +19,74 @@ function miFunc (){
     }
     else if(carrera==1 && Validar(name)){
       if(anio==1){
-            window.open("Ing%20en%20computacion/ingcomputacion.html")
-        } else if (anio==2) { window.open("ing%20computacion%202/ingcomputacion2.html")}
+            window.open("Ing%20en%20computacion/ingcomputacion.html#"+name)
+        } else if (anio==2) { window.open("ing%20computacion%202/ingcomputacion2.html#"+name)}
       else if(anio!=1 || anio!=2) {alert('Seleccione el año porfavor')}
     }
     else if(carrera==2 && Validar(name)) {
-        if(anio==1){window.open("ingelectronica/Ingelectronica1.html")}
-        else if (anio==2){window.open("Ingelectronica%202/ingelectronica2.html")}
+        if(anio==1){window.open("ingelectronica/Ingelectronica1.html#"+name)}
+        else if (anio==2){window.open("Ingelectronica%202/ingelectronica2.html#"+name)}
         else if(anio!=1 || anio!=2) {alert('Seleccione el año porfavor')}
     }
     else if(carrera==3 && Validar(name)){
-        if(anio==1){window.open("ingensistemas1/ingensistemas1.html")}
-        else if (anio==2){window.open("ingensistemas2/ingensistemas2.html")}
+        if(anio==1){window.open("ingensistemas1/ingensistemas1.html#"+name)}
+        else if (anio==2){window.open("ingensistemas2/ingensistemas2.html#"+name)}
         else if(anio!=1 || anio!=2) {alert('Seleccione el año porfavor')}
     }
     else if (carrera!=1 && carrera != 2 && carrera!=3){alert('Seleccione la carrera porfavor')}
 
 }
+function cargarnombre(){
+    var urlcomp,nombre;
+    urlcomp=window.location.href.split("/")[5];
+    nombre=urlcomp.split("#")[1];
 
-
-window.onload= function mostrar(){
-    document.getElementById("texto").innerHTML ="tetas";
+   document.getElementById("texto").innerHTML = "¡Buenos dias " + nombre + " bienvenido a la Universidad Nacional de los Nicos!";
 }
 
-window.onload=function draw() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext('2d');
-    ctx.translate(250,200);
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
+function cargarcanvas(){
+    var canva=document.getElementById("myCanvas");
+    var ctx= canva.getContext("2d");
+
+    var altmax= canva.height;
+    var anmax=canva.width;
+
+    ctx.beginPath();
+    for(var i=0;i<altmax;){
+        ctx.moveTo(0,i);
+        ctx.lineTo(anmax,i);
+        ctx.strokeStyle = "#bbb4b4";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+
+    ctx.beginPath();
+    for(var i=0;i<anmax;){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,altmax);
+        ctx.strokeStyle = "#bbb4b4";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+    //eje X
+    ctx.beginPath();
+        ctx.moveTo(0,altmax/2);
+        ctx.lineTo(anmax,altmax/2);
+        ctx.strokeStyle = "#030303";
+        ctx.stroke();
+    ctx.closePath();
+
+    //eje y
+    ctx.beginPath();
+    ctx.moveTo(anmax/2,0);
+    ctx.lineTo(anmax/2,altmax);
+    ctx.strokeStyle = "#030303";
     ctx.stroke();
-    }
+    ctx.closePath();
 
 
-
-/**
-function miFunc() {
-    const carrera = document.getElementById("carreras");
-    const  a=carrera.options[carrera.selectedIndex].value;
-    const anio = document.getElementById("Año");
-    const b=anio.options[anio.selectedIndex].value;
-const carrera = document.getElementById("carreras").value;
-     const anio = document.getElementById("Año").value;
-
-    if (carrera===1 && anio===1){
-        window.open("Ing%20en%20computacion/ingcomputacion.html", new.target);
-    } else if (carrera===1 && anio===2){
-        window.open("ing%20computacion%202/ingcomputacion2.html")
-    }
-
-
-
-}**/
+}
